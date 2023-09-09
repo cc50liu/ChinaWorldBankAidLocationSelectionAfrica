6fab30197293d2b3542a34d0cf71c603a08dd417
Cindy Conlin <101796166+cc50liu@users.noreply.github.com>
2023-09-09 14:43:27 +0200

    Script to fix filenames

---
+++ scripts/rename_tfrec_cnn_shuf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# repair file names for a run I called tfrec_cnn that should have been tfrec_cnn_shuf 
+# (funder/sector both_110 was correct, so I moved it to a "correct" directory while I did this
+# cd ./results/tfrec_cnn_shuf
+# Loop through each Causal Salience Map in the directory
+for file in ./*tfrec_cnn*; do
+  # Check if the file exists
+  if [ -e "$file" ]; then
+    # Extract the filename without the directory path
+    filename=$(basename "$file")
+    
+    # Replace "tfrec_cnn" with "tfrec_cnn_shuf" in the filename
+    new_filename="${filename//tfrec_cnn/tfrec_cnn_shuf}"
+    
+    # Rename the file
+    mv "$file" "./$new_filename"
+    
+    echo "Renamed: $filename to $new_filename"
+  fi
+done
