c066c6c562fca1d43f92fae16ae04ca882f8b714
Cindy Conlin <101796166+cc50liu@users.noreply.github.com>
2023-09-20 15:19:04 +0200

    Script for collapsed time cnn run

---
+++ scripts/run_tfrec_cnn_collapsed.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Submit slurm scripts every minute
+submit_and_wait() {
+  sbatch "$1" "$2" "$3" "$4" "$5"  # Submit the Slurm script with parameters
+  sleep 1m     # Sleep for 1 minute
+}
+
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_110 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_110 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_110 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_120 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_120 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_120 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_140 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_140 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_140 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_150 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_150 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_150 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_160 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_160 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_160 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_210 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_210 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_210 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_220 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_220 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_220 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_230 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_230 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_230 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_240 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm both_310 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_310 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_320 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_330 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm wb_410 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_430 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_520 tfrec_cnn_collapsed 1000 collapsed
+submit_and_wait call_CI_Conf_tfrec_cnn_collapsed.slurm ch_700 tfrec_cnn_collapsed 1000 collapsed
