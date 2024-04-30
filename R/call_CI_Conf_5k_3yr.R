4b90f5ee4420aef16524355e275cab518b06f25e
Cindy Conlin <101796166+cc50liu@users.noreply.github.com>
2024-04-30 08:46:08 -0600

    Restore Fixed Effects

--- R/call_CI_Conf_5k_3yr.R
+++ R/call_CI_Conf_5k_3yr.R
@@ -20,7 +20,7 @@ vision_backbone <- args[5]
 
 #uncomment to test
 # fund_sect_param <- "wb_120"
-# fund_sect_param <- "ch_430"
+# fund_sect_param <- "ch_140"
 # run <- "emb_5k_3yr"
 # iterations <- 15
 # time_approach <- "3yr"   #other option: "annual"
@@ -356,7 +356,41 @@ if (treat_count < 100) {
     print(paste0("Stopping because incomplete cases.  See ./data/interim/input_",
                  run,"_",fund_sect_param,".csv"))
   } else {
-    # conf_matrix <- cbind(
+    conf_matrix <- cbind(
+      as.matrix(data.frame(
+        "first_year_group"           =input_df$first_year_group - 2001,
+        #"first_year_group_squared"   =(input_df$first_year_group - 2001)^2,
+        "log_pc_nl_pre_oda"          =input_df$log_pc_nl_pre_oda,           #scene level
+        "log_avg_min_to_city"        =input_df$log_avg_min_to_city,         #scene level
+        "log_avg_pop_dens"           =input_df$log_avg_pop_dens,            #scene level
+        "log_dist_km_to_gold"        =input_df$log_dist_km_to_gold,         #scene level
+        "log_dist_km_to_gems"        =input_df$log_dist_km_to_gems,         #scene level
+        "log_dist_km_to_dia"         =input_df$log_dist_km_to_dia,          #scene level
+        "log_dist_km_to_petro"       =input_df$log_dist_km_to_petro,        #scene level
+        "log_treated_other_funder_n" =input_df$log_treated_other_funder_n,  #inherited from ADM2
+        "log_ch_loan_proj_n"         =input_df$log_ch_loan_proj_n,          #inherited from ADM1, ADM2
+        "log_other_sect_n"           =input_df$log_other_sect_n,            #inherited from ADM2
+        "log_3yr_pre_conflict_deaths"=input_df$log_3yr_pre_conflict_deaths, #inherited from ADM1
+        "log_disasters"              =input_df$log_disasters,               #inherited from ADM1,2,or3
+        "leader_birthplace"          =input_df$leader_birthplace,           #inherited from ADM1
+        "election_year"              =input_df$election_year,               #country level
+        "unsc_aligned_us"            =input_df$unsc_aligned_us,             #country level
+        "unsc_non_aligned_us"        =input_df$unsc_non_aligned_us,         #country level
+        "country_gini"               =input_df$country_gini,                #country level
+        "corruption_control"         =input_df$corruption_control,          #country level
+        "gov_effectiveness"          =input_df$gov_effectiveness,           #country level
+        "political_stability"        =input_df$political_stability,         #country level
+        "reg_quality"                =input_df$reg_quality,                 #country level
+        "rule_of_law"                =input_df$rule_of_law,                 #country level
+        "voice_accountability"       =input_df$voice_accountability,        #country level
+        "landsat578"                 =input_df$landsat578,                  #pre-treat image
+        "log_total_neighbor_projs"   =input_df$log_total_neighbor_projs     #neighbor ADM2s
+      )),
+      #multiple columns for adm2 fixed effects variables
+      model.matrix(~ adm2 - 1, input_df)
+    )
+    
+    # conf_matrix <- 
     #   as.matrix(data.frame(
     #     "first_year_group"           =input_df$first_year_group - 2001,
     #     #"first_year_group_squared"   =(input_df$first_year_group - 2001)^2,
@@ -385,41 +419,7 @@ if (treat_count < 100) {
     #     "voice_accountability"       =input_df$voice_accountability,        #country level 
     #     "landsat578"                 =input_df$landsat578,                  #pre-treat image 
     #     "log_total_neighbor_projs"   =input_df$log_total_neighbor_projs     #neighbor ADM2s
-    #   )),
-    #   #multiple columns for adm2 fixed effects variables
-    #   model.matrix(~ adm2 - 1, input_df)
-    # )
-    
-    conf_matrix <- 
-      as.matrix(data.frame(
-        "first_year_group"           =input_df$first_year_group - 2001,
-        #"first_year_group_squared"   =(input_df$first_year_group - 2001)^2,
-        "log_pc_nl_pre_oda"          =input_df$log_pc_nl_pre_oda,           #scene level
-        "log_avg_min_to_city"        =input_df$log_avg_min_to_city,         #scene level
-        "log_avg_pop_dens"           =input_df$log_avg_pop_dens,            #scene level
-        "log_dist_km_to_gold"        =input_df$log_dist_km_to_gold,         #scene level
-        "log_dist_km_to_gems"        =input_df$log_dist_km_to_gems,         #scene level
-        "log_dist_km_to_dia"         =input_df$log_dist_km_to_dia,          #scene level
-        "log_dist_km_to_petro"       =input_df$log_dist_km_to_petro,        #scene level
-        "log_treated_other_funder_n" =input_df$log_treated_other_funder_n,  #inherited from ADM2
-        "log_ch_loan_proj_n"         =input_df$log_ch_loan_proj_n,          #inherited from ADM1, ADM2
-        "log_other_sect_n"           =input_df$log_other_sect_n,            #inherited from ADM2
-        "log_3yr_pre_conflict_deaths"=input_df$log_3yr_pre_conflict_deaths, #inherited from ADM1
-        "log_disasters"              =input_df$log_disasters,               #inherited from ADM1,2,or3			 
-        "leader_birthplace"          =input_df$leader_birthplace,           #inherited from ADM1
-        "election_year"              =input_df$election_year,               #country level
-        "unsc_aligned_us"            =input_df$unsc_aligned_us,             #country level
-        "unsc_non_aligned_us"        =input_df$unsc_non_aligned_us,         #country level
-        "country_gini"               =input_df$country_gini,                #country level
-        "corruption_control"         =input_df$corruption_control,          #country level
-        "gov_effectiveness"          =input_df$gov_effectiveness,           #country level 
-        "political_stability"        =input_df$political_stability,         #country level
-        "reg_quality"                =input_df$reg_quality,                 #country level
-        "rule_of_law"                =input_df$rule_of_law,                 #country level           
-        "voice_accountability"       =input_df$voice_accountability,        #country level 
-        "landsat578"                 =input_df$landsat578,                  #pre-treat image 
-        "log_total_neighbor_projs"   =input_df$log_total_neighbor_projs     #neighbor ADM2s
-      ))
+    #   ))
     
     #remove any columns that have 0 standard deviation before passing to function
     before_cols <-  colnames(conf_matrix)
